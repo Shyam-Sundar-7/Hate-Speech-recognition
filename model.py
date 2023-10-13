0ae3771ae7e44dc7bf0b87f6c06c17bfc3eb07e0
Shyam-Sundar-7 <mailshyamsundar.2022@gmail.com>
2023-10-13 16:42:48 +0530

    wandb and torchmetrics

--- model.py
+++ model.py
@@ -3,9 +3,13 @@ import torch.nn as nn
 import lightning.pytorch as pl
 import torch.nn.functional as F
 from transformers import AutoModel
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,confusion_matrix
 import torchmetrics
-
+import wandb
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
 
 class HateModel(pl.LightningModule):
     def __init__(self, model_name="google/bert_uncased_L-2_H-128_A-2", lr=1e-2):
@@ -18,6 +22,17 @@ class HateModel(pl.LightningModule):
         # Freeze the pre-trained BERT model
         for param in self.bert.parameters():
             param.requires_grad = False
+        self.train_accuracy_metric = torchmetrics.Accuracy()
+        self.val_accuracy_metric = torchmetrics.Accuracy()
+        self.f1_metric = torchmetrics.F1(num_classes=self.num_classes)
+        self.precision_macro_metric = torchmetrics.Precision(
+            average="macro", num_classes=self.num_classes
+        )
+        self.recall_macro_metric = torchmetrics.Recall(
+            average="macro", num_classes=self.num_classes
+        )
+        self.precision_micro_metric = torchmetrics.Precision(average="micro")
+        self.recall_micro_metric = torchmetrics.Recall(average="micro")
         
 
     def forward(self, input_ids, attention_mask):
@@ -30,17 +45,67 @@ class HateModel(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         logits = self.forward(batch["input_ids"], batch["attention_mask"])
         loss = F.cross_entropy(logits, batch["label"])
-        self.log("train_loss", loss, prog_bar=True)
+        preds = torch.argmax(logits, 1)
+        train_acc = self.train_accuracy_metric(preds, batch["label"])
+        self.log("train/loss", loss, prog_bar=True, on_epoch=True)
+        self.log("train/acc", train_acc, prog_bar=True, on_epoch=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
+        labels = batch["label"]
         logits = self.forward(batch["input_ids"], batch["attention_mask"])
         loss = F.cross_entropy(logits, batch["label"])
         _, preds = torch.max(logits, dim=1)
-        val_acc = accuracy_score(preds.cpu(), batch["label"].cpu())
-        val_acc = torch.tensor(val_acc)
-        self.log("val_loss", loss, prog_bar=True)
-        self.log("val_acc", val_acc, prog_bar=True)
 
+        # Metrics
+        valid_acc = self.val_accuracy_metric(preds, labels)
+        precision_macro = self.precision_macro_metric(preds, labels)
+        recall_macro = self.recall_macro_metric(preds, labels)
+        precision_micro = self.precision_micro_metric(preds, labels)
+        recall_micro = self.recall_micro_metric(preds, labels)
+        f1 = self.f1_metric(preds, labels)
+
+        # Logging metrics
+        self.log("valid/loss", loss, prog_bar=True, on_step=True)
+        self.log("valid/acc", valid_acc, prog_bar=True, on_epoch=True)
+        self.log("valid/precision_macro", precision_macro, prog_bar=True, on_epoch=True)
+        self.log("valid/recall_macro", recall_macro, prog_bar=True, on_epoch=True)
+        self.log("valid/precision_micro", precision_micro, prog_bar=True, on_epoch=True)
+        self.log("valid/recall_micro", recall_micro, prog_bar=True, on_epoch=True)
+        self.log("valid/f1", f1, prog_bar=True, on_epoch=True)
+        return {"labels": labels, "logits": logits}
+    
+    def validation_epoch_end(self, outputs):
+        labels = torch.cat([x["labels"] for x in outputs])
+        logits = torch.cat([x["logits"] for x in outputs])
+        preds = torch.argmax(logits, 1)
+
+        ## There are multiple ways to track the metrics
+        # 1. Confusion matrix plotting using inbuilt W&B method
+        self.logger.experiment.log(
+            {
+                "conf": wandb.plot.confusion_matrix(
+                    probs=logits.numpy(), y_true=labels.numpy()
+                )
+            }
+        )
+
+        # 2. Confusion Matrix plotting using scikit-learn method
+        wandb.log({"cm": wandb.sklearn.plot_confusion_matrix(labels.numpy(), preds)})
+
+        # 3. Confusion Matric plotting using Seaborn
+        data = confusion_matrix(labels.numpy(), preds.numpy())
+        df_cm = pd.DataFrame(data, columns=np.unique(labels), index=np.unique(labels))
+        df_cm.index.name = "Actual"
+        df_cm.columns.name = "Predicted"
+        plt.figure(figsize=(7, 4))
+        plot = sns.heatmap(
+            df_cm, cmap="Blues", annot=True, annot_kws={"size": 16}
+        )  # font size
+        self.logger.experiment.log({"Confusion Matrix": wandb.Image(plot)})
+
+        self.logger.experiment.log(
+            {"roc": wandb.plot.roc_curve(labels.numpy(), logits.numpy())}
+        )
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.hparams["lr"])
