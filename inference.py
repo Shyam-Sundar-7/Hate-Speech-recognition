02bf10b58b78800d33dd80445d6f5fb6d118eac1
Shyam-Sundar-7 <mailshyamsundar.2022@gmail.com>
2023-10-13 16:16:32 +0530

    inference pipeline completed

--- inference.py
+++ inference.py
@@ -11,14 +11,14 @@ class HatePredictor:
         self.model.freeze()
         self.processor = DataModule()
         self.softmax = torch.nn.Softmax(dim=0)
-        self.lables = ["unacceptable", "acceptable"]
+        self.lables = ["Normal Speech", "Toxic Speech"]
 
     def predict(self, text):
-        inference_sample = {"sentence": text}
+        inference_sample = {"comment_text": text}
         processed = self.processor.tokenize_data(inference_sample)
         logits = self.model(
-            torch.tensor([processed["input_ids"]]).to(self.model.device()),
-            torch.tensor([processed["attention_mask"]]).to(self.model.device()),
+            torch.tensor([processed["input_ids"]]).to(self.model.device),
+            torch.tensor([processed["attention_mask"]]).to(self.model.device),
         )
         scores = self.softmax(logits[0]).tolist()
         predictions = []
@@ -29,6 +29,6 @@ class HatePredictor:
 
 if __name__ == "__main__":
     sentence = "The boy is sitting on a bench"
-    predictor = HatePredictor("./models/epoch=0-step=267.ckpt")
+    predictor = HatePredictor("./models/hate_model.ckpt")
     print(predictor.predict(sentence))
 
